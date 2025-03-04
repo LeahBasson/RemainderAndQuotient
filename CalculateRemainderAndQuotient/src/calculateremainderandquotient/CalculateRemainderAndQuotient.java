0e55e08a34032ce15b0a7a262e6ecfe7fc231a18
LeahBasson <leahbasson54@gmail.com>
2025-03-05 00:42:26 +0200

    :tada:

---
+++ CalculateRemainderAndQuotient/src/calculateremainderandquotient/CalculateRemainderAndQuotient.java
@@ -0,0 +1,17 @@
+
+package calculateremainderandquotient;
+
+
+public class CalculateRemainderAndQuotient {
+
+    
+    public static void main(String[] args) {
+        double num1 = 17;
+        double num2 = 3;
+        double calculation = num1/num2;
+        
+        System.out.println("Quotient and Remainder: " + calculation);
+        
+    }
+    
+}
